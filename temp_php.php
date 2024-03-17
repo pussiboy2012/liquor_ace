beadcaaebc69168025c5a74933ee6d63c09180d9
_Заклинатель_Роман_ <91064501+pussiboy2012@users.noreply.github.com>
2024-03-17 14:30:46 +0300

    Delete temp_php.php

--- temp_php.php
+++
@@ -1,5 +0,0 @@
-<?
-include './auth_vk/config.php';
-?>
-<!-- https://dev.vk.com/api/access-token/authcode-flow-user -->
-<a href="https://oauth.vk.com/authorize?client_id=<?=ID?>&display=page&redirect_uri=<?=URL?>&scope=photos&response_type=code&v=5.131" target="_blank">VK AUTHORIZE</a>
